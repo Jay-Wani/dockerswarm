6d847b50e46abebfee784844810a94a248f18991
Junaid Wani <w.m.junaid@gmail.com>
2017-01-15 19:24:44 +0530

    Update swarm-resize.sh

--- swarm-gcp/swarm-resize.sh
+++ swarm-gcp/swarm-resize.sh
@@ -12,8 +12,8 @@
 #  limitations under the License.
 
 # get cluster info from options.yaml
-PREFIX=$(awk '{for(i=1;i<=NF;i++) if ($i=="prefix:") print $(i+1)}' options.yaml)
-ZONE=$(awk '{for(i=1;i<=NF;i++) if ($i=="zone:") print $(i+1)}' options.yaml)
+PREFIX=$(awk '{for(i=1;i<=NF;i++) if ($i=="prefix:") print $(i+1)}' config.yaml)
+ZONE=$(awk '{for(i=1;i<=NF;i++) if ($i=="zone:") print $(i+1)}' config.yaml)
 PROJECT_ID=$(gcloud config list project | awk 'FNR ==2 { print $3 }')
 
 echo "Resizing Swarm Deplyoment"
